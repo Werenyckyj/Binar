c89a3ef6fc74d36556a2a1565b4ffcd1a2584b48
werenyckyj.vo.2019 <werenyckyj.vo.2019@UC408PC17>
2022-11-02 13:16:23 +0100

    random numbers

--- Binar 2.0/Program.cs
+++ Binar 2.0/Program.cs
@@ -8,25 +8,42 @@ namespace Binar_2._0
 {
     class Program
     {
-        static int[] numbers = new int[1000000];
+        static Random rc = new Random();
+        static int[] numbers = new int[10000];
+        static int last = 0;
         static void Main(string[] args)
         {
             LoadIntArray();
-            FindIndexOf(169841, numbers, 0, numbers.Length);
-
-            Console.ReadLine();
+            while (true)
+            {
+                Console.Write("Zadej číslo, které chceš najít: ");
+                int find = int.Parse(Console.ReadLine());
+                FindIndexOf(find, numbers, 0, numbers.Length);
+                Console.WriteLine();
+            }
         }
         public static void LoadIntArray()
         {
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                numbers[i] = i + 1;
+                int n = rc.Next(100000);
+                if (numbers.Contains<int>(n))
+                {
+                    i--;
+                }
+                else
+                {
+                    numbers[i] = n;
+                    Console.WriteLine($"Číslo {n} přidáno");
+                }
             }
+            Array.Sort(numbers);
+            Console.WriteLine("Vygenerováno pole o velikosti " + numbers.Length);
         }
         static void FindIndexOf(int find, int[] array, int from, int to)
         {
             int halfOfArray = 0;
-            if (find > array.Length || find <= 0)
+            if (find > 100000 || find <= 0)
             {
                 Console.WriteLine("Číslo není v poli");
                 return;
@@ -40,17 +57,20 @@ namespace Binar_2._0
             {
                 Console.WriteLine($"Číslo: {array[halfOfArray]} je na indexu {halfOfArray}");
             }
+            else if (last == to - from)
+            {
+                Console.WriteLine("Číslo není v poli");
+                return;
+            }
             else if (array[halfOfArray] > find)
             {
+                last = to - from;
                 FindIndexOf(find, array, from, halfOfArray - 1);
             }
             else if (array[halfOfArray] < find)
             {
+                last = to - from;
                 FindIndexOf(find, array, halfOfArray + 1, to);
-            }
-            else
-            {
-                Console.WriteLine("Číslo není v poli");
             }
         }
     }
